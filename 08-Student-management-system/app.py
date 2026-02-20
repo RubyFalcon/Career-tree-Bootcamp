0440a86d4052fc329ecc388dd650d319a6a82978
Tupay <61619620+RubyFalcon@users.noreply.github.com>
2026-02-20 17:42:52 +0000

    studend and school class added

--- 08-Student-management-system/app.py
+++ 08-Student-management-system/app.py
@@ -12,17 +12,28 @@ class School:
     
     def get_students(self):
         return self.students
+    def find_student_by_id(self, id):
+        for student in self.students:
+            if id == student.id:
+                print("You found me")
+
 
 
 def main():
     print("got here")
     my_school = School()
 
-    my_school.add_student(Student(1,"John","Doe", 16, "123 Adress", 11))
-    my_school.add_student(Student(2,"Joe","Weller",15,"145 Address", 10))
-    my_school.add_student(Student(3,"Super", "Man",17,"566 noting hill", 13))
+    my_school.add_student(Student(1,"John","Doe", 16, "123 Adress", 11,"maths", "english"))
+    my_school.add_student(Student(2,"Joe","Weller",15,"145 Address", 10,"Economics", "English",))
+    my_school.add_student(Student(3,"Super", "Man",17,"566 noting hill", 13,"Maths"))
 
     print(my_school.get_students())
+    my_school.find_student_by_id(1)
 
 if __name__ == "__main__": #name= main means we are running this specific python file
     main()
+
+
+# todo: find scores by id
+
+# find a student by name - match all names
--- 08-Student-management-system/student.py
+++ 08-Student-management-system/student.py
@@ -1,15 +1,20 @@
 
 class Student: 
-    def __init__(self, id: int , first_name: str, last_name:str, age:int, address:str, school_year:int):
+    def __init__(self, id: int , first_name: str, last_name:str, age:int, address:str, school_year:int,scores:dict = {},*subjects, **details):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.address = address 
         self.school_year = school_year
+        self.subjects = list(subjects)
+        self.details = details
+        self.scores = scores
 
     def __str__(self):
-        return f"{self.id}: {self.first_name} {self.last_name}, Age {self.age}, Year {self.school_year}"
-    
+        return f"id:{self.id}\n name: {self.first_name} {self.last_name} \n, Age {self.age} \nYear {self.school_year} \nScored: {self.scores} \nSubjects: {self.subjects} \ndetails: {self.details} "
+
     def __repr__(self):
         return self.__str__()
+    def add_score(self, subject,score):
+        self.scores[subject] = score
