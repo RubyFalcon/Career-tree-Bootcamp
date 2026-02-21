739d3c507b8c54e4b6420266e8b1957bb70626dc
Tupay <61619620+RubyFalcon@users.noreply.github.com>
2026-02-21 11:16:03 +0000

    created data methods for students

--- 08-Student-management-system/app.py
+++ 08-Student-management-system/app.py
@@ -4,7 +4,7 @@
 from student import Student
 
 class School:
-    def __init__(self, students: list | None = None):
+    def __init__(self, students: list[Student] | None = None):
         self.students = students if students is not None else []
 
     def add_student(self,student:Student):
@@ -12,11 +12,35 @@ class School:
     
     def get_students(self):
         return self.students
-    def find_student_by_id(self, id):
+    def find_student_by_id(self, student_id):
         for student in self.students:
-            if id == student.id:
-                print("You found me")
+            if student_id == student.id:
+                return student
+        else:
+            return None
+        
+    def add_student_score(self,student_id,subject,score):
+        student = self.find_student_by_id(student_id)
+        if student is not None:
+            student.add_score(subject, score)
+        else:
+            print("Student not found")
+            return
+    def find_student_by_name(self, name):
+        return [ student for student in self.students
+                if student.first_name.lower() == name.lower()]
+    def get_average_scores(self, student_id):
+        student = self.find_student_by_id(student_id)
+        if student is None:
+            print("Student not found")
+            return None
 
+        if not student.scores:  
+            return 0
+
+        total = sum(student.scores.values())
+        count = len(student.scores)
+        return f"{student.first_name}'s average score is : {total / count}"
 
 
 def main():
@@ -24,16 +48,20 @@ def main():
     my_school = School()
 
     my_school.add_student(Student(1,"John","Doe", 16, "123 Adress", 11,"maths", "english"))
+    my_school.add_student(Student(1,"John","Smith", 13, "123 Adress", 8,"maths", "english"))
     my_school.add_student(Student(2,"Joe","Weller",15,"145 Address", 10,"Economics", "English",))
     my_school.add_student(Student(3,"Super", "Man",17,"566 noting hill", 13,"Maths"))
 
-    print(my_school.get_students())
+    # print(my_school.get_students())
     my_school.find_student_by_id(1)
-
+    my_school.add_student_score(1,"Maths", 90)
+    my_school.add_student_score(1,"English", 60)
+    # print(my_school.find_student_by_id(1).scores)
+    print(my_school.find_student_by_name("John"))
+    print(my_school.get_average_scores(1))
 if __name__ == "__main__": #name= main means we are running this specific python file
     main()
 
 
-# todo: find scores by id
 
 # find a student by name - match all names
--- 08-Student-management-system/student.py
+++ 08-Student-management-system/student.py
@@ -1,6 +1,16 @@
 
 class Student: 
-    def __init__(self, id: int , first_name: str, last_name:str, age:int, address:str, school_year:int,scores:dict = {},*subjects, **details):
+    def __init__(
+            self, 
+            id: int,
+            first_name: str,
+            last_name:str,
+            age:int, address:str,
+            school_year:int,
+            scores:dict = {},
+            *subjects,
+            **details,
+            ):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
@@ -9,10 +19,10 @@ class Student:
         self.school_year = school_year
         self.subjects = list(subjects)
         self.details = details
-        self.scores = scores
+        self.scores = {}
 
     def __str__(self):
-        return f"id:{self.id}\n name: {self.first_name} {self.last_name} \n, Age {self.age} \nYear {self.school_year} \nScored: {self.scores} \nSubjects: {self.subjects} \ndetails: {self.details} "
+        return f"id: {self.id} \nname: {self.first_name} {self.last_name} \nAge {self.age} \nYear {self.school_year} \nScored: {self.scores} \nSubjects: {self.subjects} \ndetails: {self.details} "
 
     def __repr__(self):
         return self.__str__()
