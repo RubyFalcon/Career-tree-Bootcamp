090ddb48c20d468391ca711a471cabefccaf51f7
Tupay <61619620+RubyFalcon@users.noreply.github.com>
2026-02-27 15:23:26 +0000

    ensures save_transaction will take a flaot as amount

--- Finance_Tracker/transactions.py
+++ Finance_Tracker/transactions.py
@@ -46,7 +46,7 @@ def load_transactions(filename=DATA_FILE):
                 finances["expenses"] += amount
 
 # save transactions
-def save_transaction(transaction_type, category, amount, filename=DATA_FILE):
+def save_transaction(transaction_type, category, converted_amount: float, filename=DATA_FILE):
     """
     Appends a single transaction to a text file as CSV:
     timestamp,type,category,amount
@@ -54,12 +54,13 @@ def save_transaction(transaction_type, category, amount, filename=DATA_FILE):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     # Always write with newline at end; format amount to 2dp for £
-    line = f"{timestamp},{transaction_type},{category},{amount:.2f}\n"
+    line = f"{timestamp},{transaction_type},{category},{converted_amount:.2f}\n"
 
     with open(filename, "a", encoding="utf-8") as f:
         f.write(line)
 
-    print(f"{transaction_type} of £{amount: .2f} recorded successfully")
+    print(f"{transaction_type} of £{converted_amount: .2f} recorded successfully")
+
 def add_income():
     """Add a new income transaction"""
     categories = ["salary", "freelance", "gift"]
